d5d36efbfd72db727741bf479503dffae390ccb0
straris <straris@hotmail.com>
2016-01-17 01:58:49 +0100

    Delete connect_to_mysql.php

--- connect_to_mysql.php
+++
@@ -1,5 +0,0 @@
-<?php 
-// place your DB_host, UserName, Password, and DB_Name below where shown
-mysql_connect("localhost","root","killer") or die ("Could not connect.");
-mysql_select_db("radiosite") or die ("no database");
-?>
